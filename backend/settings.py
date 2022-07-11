e5c642c3e8471a96c67669d35e23669cc91acbf0
gatsbyjames <gatsby1992@naver.com>
2022-07-11 09:06:15 +0900

    fix admin css

--- backend/settings.py
+++ backend/settings.py
@@ -188,7 +188,7 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = BASE_DIR / 'static/images'
 # 얘가 사진 업로드 경로 정하는애
 
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
